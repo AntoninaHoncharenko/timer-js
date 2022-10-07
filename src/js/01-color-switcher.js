7216ee059dabed73de9793af8c15a3082cdd6824
AntoninaHoncharenko <g.antonina.vl@gmail.com>
2022-10-07 17:32:52 +0300

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startBtnRef = document.querySelector('[data-start]');
+const stoptBtnRef = document.querySelector('[data-stop]');
+const bodyRef = document.querySelector('body');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startBtnRef.addEventListener('click', onStartBtnClick);
+stoptBtnRef.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
+  bodyRef.style.backgroundColor = getRandomHexColor();
+  timerId = setInterval(() => {
+    bodyRef.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtnRef.disabled = true;
+  stoptBtnRef.disabled = false;
+}
+
+function onStopBtnClick() {
+  clearInterval(timerId);
+  startBtnRef.disabled = false;
+  stoptBtnRef.disabled = true;
+}
